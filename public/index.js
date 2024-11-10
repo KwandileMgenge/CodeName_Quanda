023c66991baf0038112f2b8167e84d889ecbc33a
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2024-11-10 03:35:01 +0200

    completed portfolio

---
+++ public/index.js
@@ -0,0 +1,19 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("logo").classList.toggle("hidden")
+    document.getElementById("mobile-menu").classList.toggle("hidden")
+})
+
+document.getElementById("home-nav").addEventListener("click", function() {
+    document.getElementById("logo").classList.toggle("hidden")
+    document.getElementById("mobile-menu").classList.toggle("hidden")
+})
+
+document.getElementById("projects-nav").addEventListener("click", function() {
+    document.getElementById("logo").classList.toggle("hidden")
+    document.getElementById("mobile-menu").classList.toggle("hidden")
+})
+
+document.getElementById("about-nav").addEventListener("click", function() {
+    document.getElementById("logo").classList.toggle("hidden")
+    document.getElementById("mobile-menu").classList.toggle("hidden")
+})
